65cd457aabcc32b6e00a5a088c1bb8cc5a9ae90e
mahmoudRagab7 <mahmoud.ragab187@gmail.com>
2026-05-09 21:52:41 +0300

    adding base in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +8,5 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  base: process.env.VITE_BASE_PATH || "/axis-portfolio",
 })
